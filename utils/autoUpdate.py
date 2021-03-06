e560a112e9b23eb9e5944709684536c9af90360c
Dragon1573 <Legend_1949@126.com>
2021-03-06 21:08:24 +0800

    Fix: Update utils/autoUpdate.py
    Append some output commands to the scipt, in order to show details in GitHub
    Actions.
    
    Signed-off-by: Dragon1573 <Legend_1949@126.com>

--- utils/autoUpdate.py
+++ utils/autoUpdate.py
@@ -1,17 +1,31 @@
-﻿# Auto Update Scoop Bucket
+# Auto Update Scoop Bucket
 #
 # Author: Dragon1573
 
 from subprocess import getoutput
 
 if __name__ == "__main__":
-    manifests = getoutput('ls bucket').splitlines()
+    # 获取所有 App Manifests
+    manifests = getoutput("ls bucket").splitlines()
     for manifest in manifests:
-        app = manifest.rstrip('.json')
-        response = getoutput(f'pwsh .\\bin\\checkver.ps1 -U {app}').splitlines()
-        if (len(response) > 1 and response[1].endswith('autoupdate available') and
-            response[-1].startswith('Writing updated') and
-            response[-1].endswith('manifest')):
-            version = response[1].split(' ')[1]
-            getoutput('git add .')
-            getoutput(f'git commit -m "{app}: Update to version {version}"')
+        # 提取 App Name
+        app = manifest.rstrip(".json")
+        # 检查更新
+        print(f'Updating {app} ...')
+        response = getoutput(f"pwsh .\\bin\\checkver.ps1 -U {app}")
+        print(response)
+        response = response.splitlines()
+        if (
+            len(response) > 1 and response[1].endswith("autoupdate available")
+            and response[-1].startswith("Writing updated")
+            and response[-1].endswith("manifest")
+        ):
+            # 提取最新的版本号
+            version = response[1].split(" ")[1]
+            # 创建 Git 提交
+            print(getoutput("git add ."))
+            print(getoutput(
+                f'git commit -m "{app}: Update to version {version}"'
+            ))
+        else:
+            print(f'{app} is currently the latest version!')
